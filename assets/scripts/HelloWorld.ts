752be93dc3cb24f48f90a5692cec02b5524a6d6f
ringo-1011 <thinh.1011@enotion.io>
2026-05-19 14:04:55 +0700

    setup for test

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -1,11 +1,8 @@
 import { _decorator, Component } from 'cc';
 const { ccclass } = _decorator;
-const Message = "Hello World"; // const should be in uppercase
-@ccclass('HelloWorld')
-export class helloWorld extends Component { // class name should be in PascalCase
 
-    exampleFunction() {
-        var unusedVariable = Message; // unused variable should be removed
-    }
+@ccclass('HelloWorld')
+export class HelloWorld extends Component {
+
 }
 
