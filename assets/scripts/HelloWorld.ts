1469d8e47aa2c79820a69b6f80828c5fc10c14a8
ringo-1011 <thinh.1011@enotion.io>
2026-05-19 11:27:21 +0700

    setup for record

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -4,9 +4,5 @@ const Message = "Hello World"; // const should be in uppercase
 @ccclass('HelloWorld')
 export class helloWorld extends Component { // class name should be in PascalCase
 
-    exampleFunction() { //funciton name should be in camelCase
-        var unusedVariable = Message; // unused variable should be removed
-    }
-    
 }
 
