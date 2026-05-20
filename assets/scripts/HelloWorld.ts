aaac6b82236550ab4417aab7a3ddf32b1749f510
ringo-1011 <thinh.1011@enotion.io>
2026-05-20 10:57:03 +0700

    test

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -5,7 +5,7 @@ const { ccclass } = _decorator;
 export class HelloWorld extends Component {
 
     start() {
-        console.log('tieng han quoc khong bo dau');
+        console.log('đây là tiếng hàn quốc');
     }
 
 }
