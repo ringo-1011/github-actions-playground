667b6d38afd5239b92ac923359b5ddec990cfae8
ringo-1011 <thinh.1011@enotion.io>
2026-05-20 13:42:42 +0700

    tét

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -1,4 +1,5 @@
 import { _decorator, Component } from 'cc';
+import languageDetect from 'languagedetect';
 const { ccclass } = _decorator;
 
 @ccclass('HelloWorld')
@@ -6,6 +7,11 @@ export class HelloWorld extends Component {
 
     start() {
         console.log('Đây là một câu tiếng Việt hoàn chỉnh để kiểm tra hệ thống');
+        console.log('xin chao');
+        console.log('tieng viet khong dau');
+        console.log('Xin Chào');
+        console.log('Anh Tai da noi vay thi ok di');
+
     }
 
 }
